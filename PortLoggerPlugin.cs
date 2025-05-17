62fc39b59931790cb187d3a24f2e7017fa69aa80
Víctor Morilla <vmorilla@gmail.com>
2025-05-18 00:16:52 +0200

    Fixes

--- PortLoggerPlugin.cs
+++ PortLoggerPlugin.cs
@@ -1,40 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq.Expressions;
-using System.Runtime.ConstrainedExecution;
 using System.Text;
 using Plugin;
-
-// x
-
 
 namespace PortLogger
 {
     public class PortLoggerPlugin : iPlugin
     {
         private int debugPort = 0x8080;
-        private bool startWatching = false;
-        private List<int> watchAddresses = new List<int>();
         private iCSpect cspect;
 
         private enum ReceiveState
         {
             ReceivingString,
-            ReceivingParamSize,
             ReceivingParamData
         }
 
         private ReceiveState currentState = ReceiveState.ReceivingString;
         private List<byte> stringBuffer = new List<byte>();
-        private List<object> parameters = new List<object>();
-        private Queue<string> formatSpecifiers = new Queue<string>();
+        private List<char> parameters = new List<char>();
+        private int currentParameterIndex = 0;
+        private int currentParameterLength = 0;
+        private List<string> parametersValue = new List<string>();
 
-        private int expectedParamSize = 0;
         private List<byte> paramBuffer = new List<byte>();
-
-        static int stackPointer = 0xFF3E;
-
 
         public List<sIO> Init(iCSpect c)
         {
@@ -42,19 +32,6 @@ namespace PortLogger
             var sIOs = new List<sIO> { new sIO(debugPort, eAccess.Port_Write) };
             LoadConfig();
             Log($"Plugin started on port 0x{debugPort:X4}");
-            foreach (var address in watchAddresses)
-            {
-                sIOs.Add(new sIO(address, eAccess.Memory_Write));
-                Log($"Watching address 0x{address:X4}");
-            }
-
-
-            sIOs.Add(new sIO(stackPointer, eAccess.Memory_Read));
-            sIOs.Add(new sIO(stackPointer, eAccess.Memory_Write));
-
-            // Add key press 
-            sIOs.Add(new sIO("<ctrl>g", eAccess.KeyPress, 0));
-            sIOs.Add(new sIO("<ctrl>h", eAccess.KeyPress, 1));
 
             return sIOs;
         }
@@ -71,46 +48,13 @@ namespace PortLogger
                 WriteLogPort(value);
                 return true;
             }
-            else if (watchAddresses.Contains(port) && type == eAccess.Memory_Write && startWatching)
-            {
-                Log($"Memory write attempt to 0x{port:x}... Halting");
-                cspect.Debugger(eDebugCommand.Enter);
-                return true;
-            }
-            else if (port == stackPointer && type == eAccess.Memory_Write)
-            {
-                Log($"Stack pointer 0x{stackPointer:x} writen ");                
-                return true;
-            }
 
-            //Log($"Other write attempts {port}");
             return false;
         }
 
 
         public byte Read(eAccess type, int port, int _id, out bool isvalid)
         {
-            if (type == eAccess.Memory_Read)
-            {
-                var pc = cspect.GetRegs().PC;
-                if (pc >= 0x2FC6 && pc <= 0x2FD5)
-                {
-                    // var byte0 = cspect.PeekPhysical(0x3F40); //0xFF3E);
-                    var byte0 = cspect.Peek(0xFF3E); //0xFF3E);
-                    var byte1 = cspect.PeekPhysical(0x3F41); //0xFF3F);
-                    // B6FF or B6EB
-                    if (byte1 == 0xB6 && (byte0 == 0xFF || byte0 == 0xEB))
-                    {
-                        Log("Good...");
-                    }
-                    else
-                    {
-                        Log($"Unexpected value in 0xFF3E: {byte1 * 256 + byte0:X}");
-                        cspect.Debugger(eDebugCommand.Enter);
-                    }
-
-                }
-            }
             isvalid = false;
             return 0;
         }
@@ -118,9 +62,6 @@ namespace PortLogger
 
         public bool KeyPressed(int _id)
         {
-            var enabled = _id == 1 ? "enabled" : "disabled";
-            Log($"MemWatch {enabled}.");
-            startWatching = _id == 1;
             return true;
         }
 
@@ -133,9 +74,10 @@ namespace PortLogger
                     if (value == 0)
                     {
                         ParseFormatSpecifiers();
-                        if (formatSpecifiers.Count > 0)
+                        if (parameters.Count > 0)
                         {
-                            currentState = ReceiveState.ReceivingParamSize;
+                            PrepareParameter();
+                            currentState = ReceiveState.ReceivingParamData;
                         }
                         else
                         {
@@ -148,25 +90,11 @@ namespace PortLogger
                     }
                     break;
 
-                case ReceiveState.ReceivingParamSize:
-                    expectedParamSize = value;
-                    paramBuffer.Clear();
-                    currentState = ReceiveState.ReceivingParamData;
-                    break;
-
                 case ReceiveState.ReceivingParamData:
                     paramBuffer.Add(value);
-                    if (paramBuffer.Count >= expectedParamSize)
+                    if (--currentParameterLength == 0)
                     {
-                        ProcessParameter();
-                        if (parameters.Count >= formatSpecifiers.Count)
-                        {
-                            FlushMessage();
-                        }
-                        else
-                        {
-                            currentState = ReceiveState.ReceivingParamSize;
-                        }
+                        ParseCurrentParameter();
                     }
                     break;
             }
@@ -190,26 +118,62 @@ namespace PortLogger
                     if (port >= 0)
                     {
                         debugPort = port;
-                        Log($"Loaded config: DebugPort=0x{debugPort:X}");
                     }
 
-                }
-                else if (line.StartsWith("WatchAddress="))
-                {
-                    var numberStrs = line.Substring("WatchAddress=".Length).Split(',');
-
-                    // Parse the numbers based on their format
-                    int address = ParseIntWithHexSupport(numberStrs[0]);
-                    int length = numberStrs.Length > 1 ? ParseIntWithHexSupport(numberStrs[1]) : 1;
-
-                    Log($"Loaded config: WatchAddress={address:X},{length}");
-
-                    for (int i = 0; i < length; i++)
-                    {
-                        watchAddresses.Add(address + i);
-                    }
                 }
             }
+        }
+
+
+        private void PrepareParameter()
+        {
+            var currentParameter = parameters[this.currentParameterIndex];
+            this.paramBuffer.Clear();
+
+            switch (currentParameter)
+            {
+                case 'c':
+                    this.currentParameterLength = 1;
+                    break;
+                case 'f':
+                    this.currentParameterLength = 4;
+                    break;
+                default:
+                    this.currentParameterLength = 2;
+                    break;
+            }
+        }
+
+        private void ParseCurrentParameter()
+        {
+            var currentParameter = parameters[this.currentParameterIndex];
+            string value;
+
+            switch (currentParameter)
+            {
+                case 'c':
+                    value = Encoding.ASCII.GetString(paramBuffer.ToArray());
+                    break;
+                case 'f':
+                    value = "Not imp";
+                    break;
+                default:
+                    int numValue = paramBuffer[0] + paramBuffer[1] * 256;
+                    value = numValue.ToString();
+                    break;
+            }
+
+            this.parametersValue.Add(value);
+            this.currentParameterIndex++;
+            if (this.currentParameterIndex >= this.parameters.Count)
+            {
+                FlushMessage();
+            }
+            else
+            {
+                PrepareParameter();
+            }
+
         }
 
 
@@ -242,13 +206,17 @@ namespace PortLogger
             {
                 return number;
             }
-            
+
             return -1;
         }
 
 
         private void ParseFormatSpecifiers()
         {
+            this.parameters = new List<char> { };
+            this.currentParameterIndex = 0;
+            this.parametersValue = new List<String> { };
+
             string str = Encoding.ASCII.GetString(stringBuffer.ToArray());
             int i = 0;
             while (i < str.Length)
@@ -258,39 +226,11 @@ namespace PortLogger
                     if (i + 1 < str.Length)
                     {
                         char spec = str[i + 1];
-                        if (spec == 'i' || spec == 'u')
-                        {
-                            formatSpecifiers.Enqueue("%" + spec);
-                            i++;
-                        }
+                        if (spec != '%') { parameters.Add(spec); }
                     }
                 }
                 i++;
             }
-        }
-
-        private void ProcessParameter()
-        {
-            object param;
-            if (expectedParamSize == 1)
-            {
-                param = (int)paramBuffer[0];
-            }
-            else if (expectedParamSize == 2)
-            {
-                param = (int)(paramBuffer[0] | (paramBuffer[1] << 8));
-            }
-            else if (expectedParamSize == 4)
-            {
-                param = DecodeMath32(paramBuffer.ToArray());
-            }
-            else
-            {
-                Log("Unsupported parameter size: " + expectedParamSize);
-                param = 0;
-            }
-
-            parameters.Add(param);
         }
 
         private float DecodeMath32(byte[] bytes)
@@ -310,7 +250,7 @@ namespace PortLogger
             try
             {
                 string formatStr = Encoding.ASCII.GetString(stringBuffer.ToArray());
-                string formattedMessage = FormatMessage(formatStr, parameters);
+                string formattedMessage = FormatMessage(formatStr, parametersValue);
                 Log(formattedMessage);
             }
             catch (Exception ex)
@@ -320,13 +260,10 @@ namespace PortLogger
 
             stringBuffer.Clear();
             parameters.Clear();
-            formatSpecifiers.Clear();
-            paramBuffer.Clear();
-            expectedParamSize = 0;
             currentState = ReceiveState.ReceivingString;
         }
 
-        private string FormatMessage(string format, List<object> args)
+        private string FormatMessage(string format, List<String> args)
         {
             StringBuilder result = new StringBuilder();
             int argIndex = 0;
@@ -334,15 +271,12 @@ namespace PortLogger
             {
                 if (format[i] == '%' && i + 1 < format.Length)
                 {
-                    char spec = format[i + 1];
-                    if ((spec == 'i' || spec == 'u') && argIndex < args.Count)
-                    {
-                        result.Append(args[argIndex++].ToString());
-                        i++; // Skip format specifier
-                    }
+                    char spec = format[++i];
+                    if (spec == '%')
+                        result.Append('%');
                     else
                     {
-                        result.Append(format[i]);
+                        result.Append(parametersValue[argIndex++]);
                     }
                 }
                 else
